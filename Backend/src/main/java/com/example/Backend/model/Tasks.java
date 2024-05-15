ffa87d687d63a83159b98f1274e3d9a955ca386d
Dina Bader <dina19bader@gmail.com>
2024-05-15 15:34:30 +0300

    Backend <> Added Lombok @data for automatic setters/getter

--- Backend/src/main/java/com/example/Backend/model/Tasks.java
+++ Backend/src/main/java/com/example/Backend/model/Tasks.java
@@ -2,10 +2,12 @@ package com.example.Backend.model;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.aspectj.weaver.loadtime.Agent;
 
 @Entity
 @Table(name="task")
+@Data
 public class Tasks {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
