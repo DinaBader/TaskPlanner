e4b6264a428b3088f6e115aaf15b6ce582225193
Dina Bader <dina19bader@gmail.com>
2024-05-15 15:53:00 +0300

    Backend <> defined methods in controller

--- Backend/src/main/java/com/example/Backend/controller/TaskController.java
+++ Backend/src/main/java/com/example/Backend/controller/TaskController.java
@@ -1,4 +1,36 @@
 package com.example.Backend.controller;
 
+import com.example.Backend.model.Tasks;
+import com.example.Backend.service.TasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    @Autowired
+    private TasksService tasksService;
+
+    @GetMapping
+    public List<Tasks> getTasks(){
+        return tasksService.getTasks();
+    }
+
+    @PostMapping
+    public Tasks createTask(Tasks task){
+        return tasksService.createTasks(task);
+    }
+
+    @PutMapping("/update")
+    public Tasks updateTask(int id,Tasks task){
+        return tasksService.updateTask(id,task);
+    }
+
+    @DeleteMapping("/delete")
+    public void DeleteTask(int id){
+         tasksService.deleteTask(id);
+    }
 }
