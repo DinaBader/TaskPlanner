3d133ec6623d39098804a4dc94dc7e78eda2dfc8
Dina Bader <dina19bader@gmail.com>
2024-05-15 15:32:42 +0300

    Backend <> extended jpa repo

--- Backend/src/main/java/com/example/Backend/repository/TasksRepository.java
+++ Backend/src/main/java/com/example/Backend/repository/TasksRepository.java
@@ -1,4 +1,7 @@
 package com.example.Backend.repository;
 
-public interface TasksRepository {
+import com.example.Backend.model.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TasksRepository extends JpaRepository<Tasks, Integer> {
 }
