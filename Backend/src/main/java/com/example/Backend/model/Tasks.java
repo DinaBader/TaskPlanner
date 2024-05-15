4e4ea0121bdc339816fdf2fbd8a5315545dae481
Dina Bader <dina19bader@gmail.com>
2024-05-15 15:31:24 +0300

    Backend <> defined tasks mode

--- Backend/src/main/java/com/example/Backend/model/Tasks.java
+++ Backend/src/main/java/com/example/Backend/model/Tasks.java
@@ -1,4 +1,15 @@
 package com.example.Backend.model;
 
+
+import jakarta.persistence.*;
+import org.aspectj.weaver.loadtime.Agent;
+
+@Entity
+@Table(name="task")
 public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String task;
 }
